2da8495e73d85684fd9957af6494c596fafff460
黄志源 <zhiyuan.huang@thisiskapok.com>
2020-04-27 00:42:54 +0800

    feat(code-push/version): wip: 编写 version_compat_tree
    基本完成 Add() 方法

--- daemon/code-push/usecase/version_compat_tree/tree.go
+++ daemon/code-push/usecase/version_compat_tree/tree.go
@@ -1,10 +1,63 @@
 package version_compat_tree
 
+import (
+	"container/list"
+	"github.com/funnyecho/code-push/pkg/semver"
+)
+
 type versionRangeTree struct {
+	tree *list.List
 }
 
 func (v *versionRangeTree) Add(entries ...IEntry) {
-	panic("implement me")
+	for i := 0; i < len(entries); {
+		entry := entries[i]
+
+		if v.tree.Len() == 0 {
+			v.tree.PushFront(entry)
+			i++
+		} else {
+			for e := v.tree.Front(); e != nil; {
+				entryInList := e.Value.(IEntry)
+
+				switch entry.CompatVersion().Compare(entryInList.CompatVersion()) {
+				case semver.CompareLargeFlag:
+					e := e.Next()
+					if e == nil {
+						v.tree.PushBack(entry)
+						i++
+					}
+				case semver.CompareEqualFlag:
+					switch entry.Version().Compare(entryInList.Version()) {
+					case semver.CompareEqualFlag:
+						e = nil
+						i++
+					case semver.CompareLessFlag:
+						e = nil
+						i++
+					case semver.CompareLargeFlag:
+						fallthrough
+					default:
+						nextElement := e.Next()
+
+						v.tree.Remove(e)
+
+						e = nextElement
+						if e == nil {
+							v.tree.PushBack(entry)
+							i++
+						}
+					}
+				case semver.CompareLessFlag:
+					fallthrough
+				default:
+					v.tree.InsertBefore(entry, e)
+					e = nil
+					i++
+				}
+			}
+		}
+	}
 }
 
 func (v *versionRangeTree) StrictCompat(anchor ICompatQueryAnchor) ICompatQueryResult {
@@ -15,6 +68,12 @@ func (v *versionRangeTree) LooseCompat(anchor ICompatQueryAnchor) ICompatQueryRe
 	panic("implement me")
 }
 
-func New() ITree {
-	return &versionRangeTree{}
+func New(entries []IEntry) ITree {
+	tree := &versionRangeTree{
+		tree: list.New(),
+	}
+
+	tree.Add(entries...)
+
+	return tree
 }
